fcfefaca1ebddcd58377f02925204541ed6ec960
Max Estrella <imaxiv@gmail.com>
2016-05-05 18:30:39 +0200

    Maven directory layout

--- src/main/com/gildedrose/GildedRose.java
+++ src/main/com/gildedrose/GildedRose.java
@@ -1,10 +1,11 @@
 package com.gildedrose;
 
+import com.gildedrose.adapter.AgedBrieAdapter;
+
 import java.util.function.Predicate;
 
 class GildedRose {
 
-    public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
@@ -30,7 +31,7 @@ class GildedRose {
             //adapter.update();
 
 
-            if (item.name.equals(AGED_BRIE)) {
+            if (item.name.equals(AgedBrieAdapter.ITEM_NAME)) {
                 decreaseSellin(item);
                 updateQualityBrie(item);
             } else if (item.name.equals(BACKSTAGE)) {
